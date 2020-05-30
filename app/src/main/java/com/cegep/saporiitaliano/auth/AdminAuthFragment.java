94c8bfddede0f3a6c95ae9aab33be9410bb33a17
Rashmeet Kaur <kaur.rashmeet1998@gmail.com>
2020-05-30 13:24:55 -0400

    Design layout for auth screen

--- app/src/main/java/com/cegep/saporiitaliano/auth/AdminAuthFragment.java
+++
@@ -1,7 +0,0 @@
-package com.cegep.saporiitaliano.auth;
-
-import androidx.fragment.app.Fragment;
-
-public class AdminAuthFragment extends Fragment {
-
-}
--- app/src/main/java/com/cegep/saporiitaliano/auth/AuthActivity.java
+++ app/src/main/java/com/cegep/saporiitaliano/auth/AuthActivity.java
@@ -1,10 +1,8 @@
 package com.cegep.saporiitaliano.auth;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
 import com.cegep.saporiitaliano.R;
-import com.google.android.material.tabs.TabLayout;
 
 public class AuthActivity extends AppCompatActivity {
 
@@ -12,11 +10,5 @@ public class AuthActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
-
-        ViewPager pager = findViewById(R.id.pager);
-        pager.setAdapter(new AuthPagerAdapter(getSupportFragmentManager()));
-
-        TabLayout tabLayout = findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(pager);
     }
 }
