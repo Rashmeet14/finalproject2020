5238c2bf119e9e74531aa448ee059e6d4a141cec
Anshul Kapoor <anshulkapoor784@yahoo.com>
2020-05-30 21:58:32 -0400

    Setup MainActivity

---
+++ app/src/main/java/com/cegep/saporiitaliano/main/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cegep.saporiitaliano.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.cegep.saporiitaliano.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
