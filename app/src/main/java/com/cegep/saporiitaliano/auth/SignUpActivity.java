9dce4401b3c4a2eb259dc2e820879f79f06abdaa
Anshul Kapoor <anshulkapoor784@yahoo.com>
2020-06-13 16:56:18 -0400

    Check for existing user while creating new user

--- app/src/main/java/com/cegep/saporiitaliano/auth/SignUpActivity.java
+++ app/src/main/java/com/cegep/saporiitaliano/auth/SignUpActivity.java
@@ -16,11 +16,19 @@ import com.cegep.saporiitaliano.model.User;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 @SuppressWarnings("ConstantConditions")
 public class SignUpActivity extends AppCompatActivity {
+
+    private TextInputEditText emailEditText;
+    private TextInputEditText addressEditText;
+    private TextInputEditText phoneNumberEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +36,10 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         final TextInputEditText nameEditText = findViewById(R.id.name_text);
-        final TextInputEditText emailEditText = findViewById(R.id.email_text);
+        emailEditText = findViewById(R.id.email_text);
         final TextInputEditText confirmEmailEditText = findViewById(R.id.confirm_email_text);
-        final TextInputEditText phoneNumberEditText = findViewById(R.id.phone_number_text);
-        final TextInputEditText addressEditText = findViewById(R.id.address_text);
+        phoneNumberEditText = findViewById(R.id.phone_number_text);
+        addressEditText = findViewById(R.id.address_text);
         final TextInputEditText passwordEditText = findViewById(R.id.password_text);
         final TextInputEditText confirmPasswordEditText = findViewById(R.id.confirm_password_text);
 
@@ -40,8 +48,8 @@ public class SignUpActivity extends AppCompatActivity {
         findViewById(R.id.create_account_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = nameEditText.getText().toString();
-                String password = passwordEditText.getText().toString();
+                final String name = nameEditText.getText().toString();
+                final String password = passwordEditText.getText().toString();
 
                 if (TextUtils.isEmpty(name) || TextUtils.isEmpty(password)) {
                     Toast.makeText(SignUpActivity.this, "Name or Password is empty", Toast.LENGTH_SHORT).show();
@@ -61,34 +69,28 @@ public class SignUpActivity extends AppCompatActivity {
                     return;
                 }
 
-                final User user = new User();
-                user.name = name;
-                user.email = emailEditText.getText().toString();
-                user.address = addressEditText.getText().toString();
-                user.phone = PhoneNumberUtils.normalizeNumber(phoneNumberEditText.getText().toString());
-                user.password = password;
-
                 DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-                DatabaseReference users = databaseReference.child("Users");
-                String newUserKey = users.push().getKey();
-                user.id = newUserKey;
-                databaseReference.child("Users").child(newUserKey).setValue(user)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-                                Toast.makeText(SignUpActivity.this, "Account created successfully", Toast.LENGTH_SHORT).show();
-                                SaporiItalianoApplication.user = user;
-                                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
-                                startActivity(intent);
-                                finish();
+                Query query = databaseReference.child("Users");
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            User user = snapshot.getValue(User.class);
+                            user.id = snapshot.getKey();
+                            if (name.equals(user.name)) {
+                                Toast.makeText(SignUpActivity.this, "User already exist. Please choose other usernamel", Toast.LENGTH_SHORT).show();
+                                return;
                             }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(SignUpActivity.this, "Failed to create new account", Toast.LENGTH_SHORT).show();
-                            }
-                        });
+                        }
+
+                        createNewUser(name, password);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Toast.makeText(SignUpActivity.this, "Failed to create new account", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
@@ -98,5 +100,37 @@ public class SignUpActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void createNewUser(String name, String password) {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        final User user = new User();
+        user.name = name;
+        user.email = emailEditText.getText().toString();
+        user.address = addressEditText.getText().toString();
+        user.phone = PhoneNumberUtils.normalizeNumber(phoneNumberEditText.getText().toString());
+        user.password = password;
+
+        DatabaseReference users = databaseReference.child("Users");
+        String newUserKey = users.push().getKey();
+        user.id = newUserKey;
+        databaseReference.child("Users").child(newUserKey).setValue(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(SignUpActivity.this, "Account created successfully", Toast.LENGTH_SHORT).show();
+                        SaporiItalianoApplication.user = user;
+                        Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(SignUpActivity.this, "Failed to create new account", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
